6682cb60cad58a49d7f2a25a1856a5f0743dd47f
Seu nome aqui <demetryus.oliveira@gmail.com>
2016-05-03 17:07:15 -0300

    Arquivo html

---
+++ Login/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+ <form action="">
+ Nome:<br />
+ <input type="text" name="nome" required="required">
+ <p>
+ Senha:<br />
+ <input type="password" name="senha" required="required">
+ <p>
+ <input type="checkbox"> Esqueceu a Senha?
+ <input type="submit" value="Entrar" required="required">
+</form>
+
+</body>
+</html>
+
+
